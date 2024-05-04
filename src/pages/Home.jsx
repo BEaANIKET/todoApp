1aa99ee8720a4673bcdea20622a35d2674e39a27
Aniket chaturvedi <aniketchaturvedi309@gmail.com>
2024-05-04 16:20:34 +0530

    add navegate on home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = ()=>{
+
+    const navigate = useNavigate();
+
+    const handleSubmit = ()=>{
+        navigate('/Dashboard')
+    } 
+
     return (
-        <div className=''>I am the Home</div>
+        <>
+            <div className=''>I am the Home</div>
+            <button onClick={ handleSubmit } className=' border-2 border-black px-3 py-3 text-center'> click to dashboard </button>
+        </>
+        
     )
 }
 
